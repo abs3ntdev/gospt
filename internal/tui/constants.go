e74a71749dc34b6d900303ec6a3176b49fa8b29b
jjohnstondev <jakester620@gmail.com>
2023-01-09 10:55:18 -0800

    make tui better

--- internal/tui/constants.go
+++ internal/tui/constants.go
@@ -7,29 +7,20 @@ import (
 	"github.com/zmb3/spotify"
 )
 
-/* CONSTANTS */
-
 var (
-	// P the current tea program
 	P *tea.Program
-	// client
+
 	Client *spotify.Client
-	// WindowSize store the size of the terminal window
+
 	WindowSize tea.WindowSizeMsg
 )
 
-/* STYLING */
-
-// DocStyle styling for viewports
 var DocStyle = lipgloss.NewStyle().Margin(0, 2)
 
-// HelpStyle styling for help context menu
 var HelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Render
 
-// ErrStyle provides styling for error messages
 var ErrStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#bd534b")).Render
 
-// AlertStyle provides styling for alert messages
 var AlertStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("62")).Render
 
 type keymap struct {
@@ -41,11 +32,10 @@ type keymap struct {
 	Quit   key.Binding
 }
 
-// Keymap reusable key mappings shared across models
 var Keymap = keymap{
 	Radio: key.NewBinding(
 		key.WithKeys("ctrl+r"),
-		key.WithHelp("ctrl+r", "create"),
+		key.WithHelp("ctrl+r", "start radio"),
 	),
 	Enter: key.NewBinding(
 		key.WithKeys("enter"),
