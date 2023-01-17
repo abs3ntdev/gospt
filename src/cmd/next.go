a758f7a199e4949bd85498cd6288f0efca5736f5
abs3nt <abs3nt@asdf.cafe>
2023-01-16 21:08:30 -0800

    fancy next

--- src/cmd/next.go
+++ src/cmd/next.go
@@ -13,11 +13,11 @@ func init() {
 }
 
 var nextCmd = &cobra.Command{
-	Use:     "next",
+	Use:     "next {amount}",
 	Aliases: []string{"n"},
 	Args:    cobra.MatchAll(cobra.RangeArgs(0, 1)),
-	Short:   "Skip to next song",
-	Long:    `Skip to next song`,
+	Short:   "Skip to next song or skip the specified number of tracks",
+	Long:    `Skip to next song of skip the specified number of tracks`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		skipAmt := 1
 		if len(args) >= 1 {
